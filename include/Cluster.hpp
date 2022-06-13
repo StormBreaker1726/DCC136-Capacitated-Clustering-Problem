90afbacbfbed77aecf1f93ab1e1d67f7b8ca2595
Joao Victor Oliveira <joaovictoroliveira1452@gmail.com>
2022-06-13 10:54:10 -0300

    Started: cluster.hpp

---
+++ include/Cluster.hpp
@@ -0,0 +1,45 @@
+#ifndef A97DC2E7_94AA_4207_9667_07BC5C5517E9
+#define A97DC2E7_94AA_4207_9667_07BC5C5517E9
+
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include <deque>
+#include "Node.hpp"
+
+using edge_t = std::vector<std::pair<int, int>>;
+
+struct Cluster
+{
+    /*             id_source, id_target                 */
+    edge_t id_nodes;
+
+    size_t upper_bound;
+    size_t lower_bound;
+
+    double cluster_cost; /* custo das arestas armazenadas */
+    float current_bound; /* custo dos nós ali armazenados */
+
+    bool insertEdge(Node* s_node, Node* t_node)
+    {
+        if(current_bound + s_node->weight() + t_node->weight() <= upper_bound)
+        {
+            std::pair<int, int> temp;
+            temp.first = s_node->id();
+            temp.second = t_node->id();
+
+            id_nodes.push_back(temp);
+
+            
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+#endif /* A97DC2E7_94AA_4207_9667_07BC5C5517E9 */
