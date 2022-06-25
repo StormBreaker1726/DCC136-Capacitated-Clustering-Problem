c4b6eddd4113687cca9669743ecb31bcefd2ee8f
Marcelo Menezes <marceloian5@gmail.com>
2022-06-24 23:35:17 -0300

    feat: nodes with their edges map

--- include/Node.hpp
+++ include/Node.hpp
@@ -1,17 +1,21 @@
 #ifndef NODE_HPP
 #define NODE_HPP
 
+#include "defines.hpp"
 #include "Edge.hpp"
 class Node
 {
     private:
         int _id;
         float _weight;
+        std::map<int, double> _edges;
     public:
         Node(int id, float weight);
 
         int id();
         float weight();
+        void insert_edge(int id_t, double weight);
+        double edge_weight(int id_t);
 };
 
 #endif // NODE_HPP
--- src/Node.cpp
+++ src/Node.cpp
@@ -12,3 +12,15 @@ int Node::id() {
 float Node::weight() {
     return this->_weight;
 }
+
+void Node::insert_edge(int id_t, double weight) {
+    this->_edges.insert({id_t, weight});
+}
+
+double Node::edge_weight(int id_t) {
+    std::map<int, double>::iterator it = this->_edges.find(id_t);
+    if (it != this->_edges.end()) {
+        return it->second;
+    }
+    return 0;
+}
